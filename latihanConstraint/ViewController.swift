a5237a22dc6b69ebccf50bbd4e5a30468523806d
farlhmd <m.farhanilhamdi@icloud.com>
2020-11-06 15:08:57 +0700

    initialCommit

--- latihanConstraint/ViewController.swift
+++ latihanConstraint/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfAngkaPertama: UITextField!
+    @IBOutlet weak var tfAngkaKedua: UITextField!
+    @IBOutlet weak var btnHitung: UIButton!
+    @IBOutlet weak var btnReset: UIButton!
+    @IBOutlet weak var lbHasil: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btnHitung.layer.cornerRadius = 10
+        btnHitung.clipsToBounds = true
+        btnReset.layer.cornerRadius = 10
+        btnReset.clipsToBounds = true
     }
 
-
+    @IBAction func btnHitungOnClick(_ sender: Any) {
+        lbHasil.text = "0"
+        if let angkaPertama = Double(tfAngkaPertama.text ?? "0") , let angkaKedua = Double(tfAngkaKedua.text ?? "0") {
+            let hasilPerkalian = angkaPertama * angkaKedua
+            lbHasil.text = "\(Int(hasilPerkalian))"
+        }
+    }
+    @IBAction func btnResetOnClick(_ sender: Any) {
+        tfAngkaPertama.text = ""
+        tfAngkaKedua.text = ""
+        lbHasil.text = "0"
+    }
+    
 }
+
 
